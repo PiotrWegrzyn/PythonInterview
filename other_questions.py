02220a8163b473800854c538c77b824a33538de6
piotrwegrzyn <piotr.wegrzyn@axpo.pl>
2023-11-24 11:15:47 +0100

    Add questions

--- other_questions.py
+++ other_questions.py
@@ -11,15 +11,18 @@
 # Mid:
 # Testing: unittest or pytest
 # Parametrize fixtures and patch
+# walrus operator
 # Typing for Dict[str, List]
 # Class A uses class B as a parameter in
+
 # Senior:
 # SOLID principles and explain at least one
 # Decorator what is it and how to write them
 # Walrus operator
 # Generator yield from and generator.send()
+
 # Hardcore: Metaclasses
 # What are they? They are magic. They're a class of a class. They change the way you initiate a class. You define internal behavior of classes used when initiated using this class.
-# Who needs them? Nobody almost xD
+# Who needs them? Almost nobody xD
 # How do you define them? UpperAttr(type)
 # Django models utilizes them to make object variables return values of fields. class method
